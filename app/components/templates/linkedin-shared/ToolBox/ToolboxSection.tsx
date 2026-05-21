9c2491369a066b12b5b9c2dcc2b914453176d80f
ZahraTaak <xerot26@yahoo.com>
2026-05-21 14:22:38 +0200

    fixed chevron

--- app/components/templates/linkedin-shared/ToolBox/ToolboxSection.tsx
+++ app/components/templates/linkedin-shared/ToolBox/ToolboxSection.tsx
@@ -54,6 +54,7 @@ export default function ToolboxSection({
                 viewBox="0 0 12 12"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
+                style={{ width: "15px" }}
               >
                 <path
                   d="M3 4.5L6 7.5L9 4.5"
